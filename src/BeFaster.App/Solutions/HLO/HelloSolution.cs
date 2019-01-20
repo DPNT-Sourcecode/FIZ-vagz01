d857c35f44288516a514c73b18405764bdb63c6d
sbx_user1059 <sbx_user1059@ip-10-128-200-220.eu-west-2.compute.internal>
2019-01-20 19:50:24 +0000

    Sun Jan 20 19:50:24 UTC 2019

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -1,12 +1,9 @@
-﻿using BeFaster.Runner.Exceptions;
-
-namespace BeFaster.App.Solutions.HLO
+﻿namespace BeFaster.App.Solutions.HLO
 {
     public static class HelloSolution
     {
         public static string Hello(string friendName)
-        {
-            throw new SolutionNotImplementedException();
-        }
+        => "Hello";
     }
 }
+
