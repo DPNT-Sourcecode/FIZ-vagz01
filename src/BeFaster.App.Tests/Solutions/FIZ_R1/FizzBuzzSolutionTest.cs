e94946716f80d9f0618b5827c9fe99501d8f2893
sbx_user1060 <sbx_user1060@ip-10-128-196-136.eu-west-2.compute.internal>
2019-01-20 20:55:58 +0000

    Sun Jan 20 20:55:58 UTC 2019

--- src/BeFaster.App.Tests/Solutions/FIZ_R1/FizzBuzzSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/FIZ_R1/FizzBuzzSolutionTest.cs
@@ -10,7 +10,6 @@ namespace BeFaster.App.Tests.Solutions.FIZ_R1
         [TestCase(9, ExpectedResult = "fizz")]
         [TestCase(10, ExpectedResult = "buzz")]
         [TestCase(15, ExpectedResult = "fizz buzz")]
-        [TestCase(22, ExpectedResult =  "22")]
         [TestCase(1, ExpectedResult = "1")]
         [TestCase(53, ExpectedResult = "fizz buzz")]
         [TestCase(90, ExpectedResult = "fizz buzz")]
@@ -22,11 +21,11 @@ namespace BeFaster.App.Tests.Solutions.FIZ_R1
             return FizzBuzzSolution.FizzBuzz(number);
         }
 
-        [TestCase(9999, ExpectedResult = "fizz deluxe")]
-        [TestCase(55, ExpectedResult = "buzz deluxe")]
-        [TestCase(555, ExpectedResult = "fizz buzz deluxe")]
+        [TestCase(9999, ExpectedResult = "fizz fake deluxe")]
+        [TestCase(55, ExpectedResult = "buzz fake deluxe")]
+        [TestCase(555, ExpectedResult = "fizz buzz fake deluxe")]
         [TestCase(1241, ExpectedResult = "1241")]
-        [TestCase(11, ExpectedResult = "deluxe")]
+        [TestCase(11, ExpectedResult = "fake deluxe")]
         [TestCase(22, ExpectedResult = "deluxe")]
         public string Fiz_R3(int number)
         {
@@ -34,5 +33,6 @@ namespace BeFaster.App.Tests.Solutions.FIZ_R1
         }
     }
 }
+
 
 
