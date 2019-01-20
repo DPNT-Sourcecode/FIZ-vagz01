6beba88e9e9ad2df0070ff71f389531b86b8eb0a
sbx_user1059 <sbx_user1059@ip-10-128-200-220.eu-west-2.compute.internal>
2019-01-20 19:53:11 +0000

    Sun Jan 20 19:53:11 UTC 2019

---
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions.HLO
+{
+    [TestFixture]
+    public class HelloSolutionTest
+    {
+        [TestCase("John", ExpectedResult = "Hello")]
+        [TestCase("Lucy", ExpectedResult = "Hello")]
+        [TestCase("", ExpectedResult = "Hello")]
+        [TestCase(null, ExpectedResult = "Hello")]
+        public string SaysHello(string name)
+            => "Hello";
+    }
+}
