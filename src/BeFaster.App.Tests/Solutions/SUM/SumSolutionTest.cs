007db033a278306420fc4a7a4af59ff871ba99dd
sbx_user1059 <sbx_user1059@ip-10-128-200-220.eu-west-2.compute.internal>
2019-01-20 19:43:50 +0000

    Sun Jan 20 19:43:50 UTC 2019

--- src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -9,18 +9,23 @@ namespace BeFaster.App.Tests.Solutions.SUM
     {
         [TestCase(1, 1, ExpectedResult = 2)]
         [TestCase(99, 99, ExpectedResult = 198)]
-        
+        [TestCase(10, 5, ExpectedResult = 15)]
         public int ComputeSum(int x, int y)
         {
             return SumSolution.Sum(x, y);
         }
 
+        [TestCase(-1, -1)]
         [TestCase(0, 0)]
+        [TestCase(10, 0)]
+        [TestCase(100, 50)]
         [TestCase(100, 100)]
+        [TestCase(101, 101)]
         public void ComputeSum_Throws(int x, int y)
         {
             Assert.That(() => SumSolution.Sum(x, y), Throws.TypeOf<ArgumentException>());
         }
     }
 }
+
 
